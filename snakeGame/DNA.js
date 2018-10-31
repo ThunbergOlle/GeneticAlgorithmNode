a5796e0426f90445db634d9526d0a6702281a238
ThunbergOlle <olle.thunberg03@gmail.com>
2018-10-31 15:09:36 +0100

    Changed the snake game.

--- snakeGame/DNA.js
+++ snakeGame/DNA.js
@@ -1,17 +1,15 @@
 generateRandom = () => {
 
-    let value1 = Math.random();
-    let value2 = Math.random()
-    if (value1 < 0.5) value1 = -1; else value1 = 1;
-    // if(value2 < 0.5) value2 = -1; else value2 = 1;
-    return value1;
+    let random = Math.floor(Math.random() * 1200);
+
+    return random;
 
 }
 class DNA {
     constructor(num) {
         this.genes = [];
-        this.x = 0;
-        this.y = 0;
+        this.x = generateRandom();
+        this.y = generateRandom();
         this.xspeed = 1;
         this.yspeed = 0;
         this.fitness = 0;
@@ -23,40 +21,27 @@ class DNA {
         fill(255);
         rect(this.x, this.y, 10, 10);
     }
-    update() {
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
-    }
-    dir() {
-        for (let i = 0; i < this.genes.length; i++) {
-            setTimeout((i) => {
-                if (generateRandom() < 1) {
-                    this.xspeed = this.genes[i];
-                } else {
-                    this.yspeed = this.genes[i];
-                }
-            }, 1000);
 
-        }
-    }
-    calcFitness(targetPhrase) { // CALCULATE THE FITNESS
+    calcFitness(cords) { // CALCULATE THE FITNESS
         let score = 0; //SETS UP SCORE VARIABLE
-        for (let i = 0; i < this.genes.length; i++) { //FOR EACH ELEMENT INSIDE THE GENES(FOR EACH CHARACTER)
-            if (this.genes[i] == targetPhrase.charAt(i)) {
-                score++; //ADD TO THE SCORE.
-            }
+        let d  = dist(cords[0], cords[1], this.x, this.y);
+
+
+        for(let i = 0; i < d; i++){
+            if(i < d) score++;
         }
-        this.fitness = score / targetPhrase.length; // GET IT TO A PERCENT VALUE
+        this.fitness = Math.floor(score);
 
     }
     crossOver(partner) {
-        let Child = new DNA(this.genes.length);
-
-        let midPoint = this.genes.length / 2;
-        for (let i = 0; i < this.genes.length; i++) {
-            if (i < midPoint) Child.genes[i] = this.genes[i];
-            else Child.genes[i] = partner.genes[i];
-        }
+        let Child = new DNA(2);
+        Child.genes[0] = partner.genes[0];
+        Child.genes[1] = this.genes[0];
+        // let midPoint = this.genes.length / 2;
+        // for (let i = 0; i < this.genes.length; i++) {
+        //     if (i < midPoint) Child.genes[i] = this.genes[i];
+        //     else Child.genes[i] = partner.genes[i];
+        // }
         return Child;
     }
 
--- snakeGame/sketch.js
+++ snakeGame/sketch.js
@@ -1,16 +1,76 @@
-let snake;
+let totalPopulation = 4;
+let generations = 0;
+let mutationRisk = 0.01;
+let population;
+let matingPool;
+let finished = false;
+let cordsTarget = [200, 200];
+let totaltFitness;
 
-function setup(){
-    createCanvas(600, 600);
-    snake = new DNA(5);
-    console.log(snake);
+let everyoneWhoHasLived = 0;
+let bestFitness = 0;
+
+function setup() {
+    display = createP("STARTING");
+    display.class("results");
+    display.position(10, 10)
+
+    population = [];
+    createCanvas(1200, 1200);
+
+    for (let i = 0; i < totalPopulation; i++) {
+        cell = new DNA(2);
+        population.push(cell);
+        everyoneWhoHasLived++;
+    }
+    console.log(population);
+
+
 }
 
-function draw(){
+function draw() {
     background(51);
-    snake.update();
-    snake.show();
-    snake.dir(0, 1);
+
+    fill(200);
+    rect(cordsTarget[0], cordsTarget[1], 20, 20);
+
+    if (finished === false) {
+
+        for (let i = 0; i < population.length; i++) {
+            population[i].show();
+            population[i].calcFitness(cordsTarget);
+            totaltFitness =+ population[i].fitness;
+            if(population[i].fitness > bestFitness) bestFitness = population[i].fitness;
+        }
+        textFont("Courier");
+        display.html("Avg fitness: " + totaltFitness / population.length +  "   Best fitness: " + bestFitness + "   Generation: " + generations);
+        matingPool = [];
+        for (let i = 0; i < population.length; i++) {
+            let element = Math.floor(population[i].fitness / 100);
+            for (let x = 0; x < element; x++) {
+                matingPool.push(population[i]);
+            }
+        }
+
+        generations++;
+
+        for (let i = 0; i < population.length; i++) {
+            let pA = Math.floor(Math.random() * matingPool.length);
+            let pB = Math.floor(Math.random() * matingPool.length);
+            parentA = matingPool[pA];
+            parentB = matingPool[pB];
+
+            let child = parentA.crossOver(parentB);
+            child.mutate(mutationRisk);
+            population[i] = child;
+            everyoneWhoHasLived++;
+            console.log(dist(cordsTarget[0], cordsTarget[1], child.x, child.y));
+            if (dist(cordsTarget[0], cordsTarget[1], child.x, child.y) < 5) {
+                console.log("Everyone who has lived: " + everyoneWhoHasLived);
+                return finished = true;
+            }
+        }
+    }
 }
 
 
@@ -27,23 +87,23 @@ function draw(){
 
 
 
-class Snake{
-    constructor(){
-        this.x = 0; 
+class Snake {
+    constructor() {
+        this.x = 0;
         this.y = 0;
         this.xspeed = 1;
         this.yspeed = 0;
     }
 
-    update(){
+    update() {
         this.x = this.x + this.xspeed;
         this.y = this.y + this.yspeed;
     }
-    dir(x, y){
+    dir(x, y) {
         this.xspeed = x;
         this.yspeed = y;
     }
-    show(){
+    show() {
         fill(255);
         rect(this.x, this.y, 10, 10);
     }
